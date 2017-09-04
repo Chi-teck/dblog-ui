9f3b03b652b126ea16d0034a7852c0579880671e
Chi-teck <Chi-teck@users.noreply.github.com>
2017-09-04 22:17:44 +0500

    Bump subscriber weight.

--- src/EventSubscriber/DblogUiRouteSubscriber.php
+++ src/EventSubscriber/DblogUiRouteSubscriber.php
@@ -3,12 +3,24 @@
 namespace Drupal\dblog_ui\EventSubscriber;
 
 use Drupal\Core\Routing\RouteSubscriberBase;
+use Drupal\Core\Routing\RoutingEvents;
 use Symfony\Component\Routing\RouteCollection;
 
 /**
  * DBlog UI route subscriber.
  */
 class DblogUiRouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    // Ensure to run after the views route subscriber
+    // @see \Drupal\views\EventSubscriber\RouteSubscriber
+    $events[RoutingEvents::ALTER] = ['onAlterRoutes', -250];
+    return $events;
+  }
+
 
   /**
    * {@inheritdoc}
